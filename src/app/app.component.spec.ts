053c81174cc6cc55fed8e22990ea24b6a838c3ad
Yoel Fernandez Suarez <yoel.fernandez@izertis.com>
2023-04-21 11:49:55 +0200

    finalizado seccion 19

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'selectoresApp'`, () => {
+  it(`should have as title 'selectores'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('selectoresApp');
+    expect(app.title).toEqual('selectores');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('selectoresApp app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('selectores app is running!');
   });
 });
